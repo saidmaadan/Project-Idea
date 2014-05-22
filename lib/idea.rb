a90458f2baafe440758bf9c1220ec56dcdec41c4
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-22 10:10:18 -0500

    Add idea.rb with module IDEA and require_relative file paths

---
+++ lib/idea.rb
@@ -0,0 +1,13 @@
+module IDEA
+end
+
+require_relative './idea/category.rb'
+require_relative './idea/db.rb'
+require_relative './idea/post.rb'
+require_relative './idea/user.rb'
+require_relative './idea/command/assign_post_to_category.rb'
+require_relative './idea/command/command.rb'
+require_relative './idea/command/create_post.rb'
+require_relative './idea/command/create_user.rb'
+require_relative './idea/command/list_post.rb'
+require_relative './idea/command/list_user.rb'
