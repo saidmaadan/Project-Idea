624cc62ed587f37656b079b2f763d5c8456569c9
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-22 11:09:43 -0500

    Add post spec and pass all tests

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper.rb'
 
-describe "category" do
+describe "post" do
 
 id = 1
 title = "My big idea"
@@ -8,9 +8,26 @@ description = "Put a function inside a function, inside a function"
 category = "Web App"
 created_at = Time.now
 
-category = IDEA::Post.new(:id=>id, :title=>title, :description=>description, :category=>category, :created_at=>created_at)
+post = IDEA::Post.new(:id=>id, :title=>title, :description=>description, :category=>category, :created_at=>created_at)
 
-  it "stores data as a hash" do
-    expect(category).to be_a kind_of(Hash)
+  it "is created with an id" do
+    expect(post.id).to eq(1)
+  end
+
+  it "is created with a title" do
+    expect(post.title).to eq("My big idea")
+  end
+
+  it "is crreated with a description" do
+    expect(post.description).to eq("Put a function inside a function, inside a function")
+  end
+
+  it "is created with a category" do
+    expect(post.category).to eq("Web App")
+  end
+
+  it "is created with a timestamp" do
+    time = Time.now.hour
+    expect(post.created_at.hour).to eq(time)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'pry'
 require './lib/idea.rb'
 include IDEA
 
-RSpec.configure do |config|
-  config.before(:each) do
-    idea.instance_variable_set(:@__db_instance, nil)
-  end
-end
+# RSpec.configure do |config|
+#   config.before(:each) do
+#     idea.instance_variable_set(:@__db_instance, nil)
+#   end
+# end
