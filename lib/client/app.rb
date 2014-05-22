26007027fd7914312f0fdd5116a4e8eaa6f3dd5f
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-22 13:05:25 -0500

    Add stylsheets, images, and public directories.  Set up Sinatra

---
+++ lib/client/app.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra'
+require 'pry'
+require '../idea.rb'
+
+set :bind, '0.0.0.0'
+
+get '/' do
+  erb :layout
+end
+
+get '/signup' do
+  # erb :signup
+end
+
+post '/signup' do
+  # puts params
+  # @name = RPS::CreateNewUser.new.run(params[:name])
+  # erb :signup
+
+end
