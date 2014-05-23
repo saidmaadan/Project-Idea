f18535820138f7afc938606db265768d41ae591b
Said Maadan <saidmaadan@gmail.com>
2014-05-23 10:09:06 -0500

    Modified user interface layout

--- lib/client/app.rb
+++ lib/client/app.rb
@@ -8,16 +8,23 @@ enable :sessions
 set :bind, '0.0.0.0'
 
 get '/' do
-  erb :layout
+  erb :index
 end
 
 get '/signup' do
   erb :signup
 end
 
+get '/signin' do
+  erb :signin
+end
 post '/signup' do
   # puts params
   # @name = RPS::CreateNewUser.new.run(params[:name])
   # erb :signup
 
+end
+
+get '/new' do
+  erb :new
 end
